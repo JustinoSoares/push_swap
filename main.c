1c96ec7815a0347a8e7531e3cac94d5d3dc57228
Justino Soares <justinocsoares123@gmail.com>
2024-08-13 13:59:02 +0100

    m

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsoares <jsoares@student.42.fr>            +#+  +:+       +#+        */
+/*   By: justinosoares <justinosoares@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 12:44:09 by jsoares           #+#    #+#             */
-/*   Updated: 2024/08/12 10:10:14 by jsoares          ###   ########.fr       */
+/*   Updated: 2024/08/12 21:37:02 by justinosoar      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,15 @@
 
 int main(int ac, char *av[])
 {
-    int i = 1;
-    int op;
+    int i;
     t_stack *stack_a = NULL;
     t_stack *stack_b = NULL;
-    
-    while (1)
+
+    i = 1;
+    while (i < ac)
     {
-        printf("digite: ");
-        scanf("%d", &op);
-        switch (op)
-        {
-        case 1:
-            printf("stack a\n");
-            ft_write(stack_a);
-            printf("stack b\n");
-            ft_write(stack_b);
-            break;
-        case 2:
-            while (i < ac)
-            {
-                append_end(&stack_a, atoi(av[i]));
-                i++;
-            }
-            break;
-        case 3:
-            push(&stack_a, &stack_b, 'a');
-            break;
-        case 4:
-            //printf("get_index: %d\n", get_index(stack_a, 6));
-            sort_any(&stack_a, &stack_b);
-            //put_on_top(&stack_a, 1, &stack_b, 1);
-            //rr(&stack_a, 'a');
-            //put_on_top_b(&stack_b, 1);
-        default:
-            break;
-        }
+        append_end(&stack_a, atoi(av[i]));
+        i++;
     }
-    
+    sort_any(&stack_a, &stack_b);
 }
--- sort.c
+++ sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jsoares <jsoares@student.42.fr>            +#+  +:+       +#+        */
+/*   By: justinosoares <justinosoares@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 11:43:05 by jsoares           #+#    #+#             */
-/*   Updated: 2024/08/12 18:16:43 by jsoares          ###   ########.fr       */
+/*   Updated: 2024/08/12 21:55:52 by justinosoar      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,12 +37,6 @@ void    sort_3(t_stack **stack)
     }  
 }
 
-
-
-
-/*
-    se a pilha tiver  mais de 3 elementos empura no maximo 2 elementos
-*/
 int ft_send_without_verify(t_stack *stack)
 {
     if (stack_size(stack) > 3)
@@ -55,7 +49,7 @@ int ft_send_without_verify(t_stack *stack)
     return (0);
 }
 
-static int *calc_cost(int   *tab, t_stack **stack_a, t_stack **stack_b, t_stack *tmp_b)
+static int *calc_cost_goo_b(int   *tab, t_stack **stack_a, t_stack **stack_b, t_stack *tmp_b)
 {
     t_aux_sort aux;
     t_stack *tmp_a;
@@ -84,7 +78,7 @@ static int *calc_cost(int   *tab, t_stack **stack_a, t_stack **stack_b, t_stack 
     return (tab);
 }
 
-void    sort_any(t_stack **stack_a, t_stack **stack_b)
+void    sort_goo_b(t_stack **stack_a, t_stack **stack_b)
 {
     t_stack *tmp_a;
     t_stack *tmp_b;
@@ -110,7 +104,7 @@ void    sort_any(t_stack **stack_a, t_stack **stack_b)
         aux.row = 0;
         while (tmp_b)
         {
-            tab[aux.row] = calc_cost(tab[aux.row], stack_a, stack_b, tmp_b);
+            tab[aux.row] = calc_cost_goo_b(tab[aux.row], stack_a, stack_b, tmp_b);
             tmp_b = tmp_b->next;
             aux.row++;
         }
@@ -118,4 +112,71 @@ void    sort_any(t_stack **stack_a, t_stack **stack_b)
         put_on_top(stack_a, tab[aux.index_cheaper][INDEX_A], stack_b, tab[aux.index_cheaper][INDEX_B]);
         push(stack_a, stack_b, 'a');
     }
+}
+
+
+static int *calc_cost_goo_a(int   *tab, t_stack **stack_a, t_stack **stack_b, t_stack *tmp_b)
+{
+    t_aux_sort aux;
+    t_stack *tmp_a;
+    t_stack *tmp_b2;
+
+    tmp_a = *stack_a;
+    tab = malloc(sizeof(int) * (SIZE_TAB + 1));
+    if (!tab)
+    {
+        allocate_error();
+    }
+    tab[VALUE_B] = tmp_b->num;
+    tab[VALUE_DEST] = next_max(tmp_a, tab[VALUE_B]);
+    tab[INDEX_A] = get_index(tmp_a, tab[VALUE_DEST]);
+    if (tab[INDEX_A] <= (stack_size(tmp_a) / 2))
+        aux.opt_a = tab[INDEX_A];
+    else
+        aux.opt_a = stack_size(tmp_a) - tab[INDEX_A];
+    tmp_b2 = *stack_b;
+    tab[INDEX_B] = get_index(tmp_b2, tab[VALUE_B]);
+    if (tab[INDEX_B] <= (stack_size(tmp_b2) / 2))
+        aux.opt_b = tab[INDEX_B];
+    else
+        aux.opt_b = stack_size(tmp_b2) - tab[3];
+    tab[COST] = aux.opt_a + aux.opt_b;
+    return (tab);
+}
+void    sort_goo_a(t_stack **stack_a, t_stack **stack_b)
+{
+    t_stack *tmp_b;
+    t_aux_sort aux;
+    int **tab;
+
+    tmp_b = *stack_b; 
+    while (stack_size(tmp_b) > 0)
+    {
+        tmp_b = *stack_b;
+        tab = malloc(sizeof(int *) * (stack_size(tmp_b) + 1));
+        if (!tab)
+            allocate_error();
+        aux.row = 0;
+        while (tmp_b)
+        {
+            tab[aux.row] = calc_cost_goo_a(tab[aux.row], stack_a, stack_b, tmp_b);
+            tmp_b = tmp_b->next;
+            aux.row++;
+        }
+        aux.index_cheaper = ft_index_cheaper(tab);
+        put_on_top(stack_a, tab[aux.index_cheaper][INDEX_A], stack_b, tab[aux.index_cheaper][INDEX_B]);
+        push(stack_b, stack_a, 'b');
+        tmp_b = *stack_b;
+        put_on_top_a(stack_a, get_index((*stack_a), min_value(*stack_a)));
+    }
+}
+
+void    sort_any(t_stack **stack_a, t_stack **stack_b)
+{
+        sort_goo_b(stack_a, stack_b);
+        sort_goo_a(stack_a, stack_b);
+        if (is_sorted(stack_a))
+            printf("OK\n");
+        else
+            printf("KO\n");
 }
